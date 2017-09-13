08941fbcf03765d1ddc6e373e781e363266fbb38
Roberto Palacios <rpalaces@csumb.edu>
2017-09-13 22:45:51 +0000

    Adding directory

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-This is something
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index of My Works </title>
+    </head>
+    <body>
+    <table style="width:100%">
+  <tr>
+    <td>Labs</td>
+    <td>Assignments</td> 
+   
+  </tr>
+  <tr>
+    <td><a href="indexlabone.html">Lab 1</a></td>
+    <td><a href="indexassignone.html">Assignment 1</td>
+  </tr>
+</table>
+    </body>
+</html>
