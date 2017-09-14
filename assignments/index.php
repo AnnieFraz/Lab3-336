1110e44f022c77251876f466551e45bd3864650b
Roberto Palacios <rpalaces@csumb.edu>
2017-09-14 00:25:57 +0000

    last index change for now

---
+++ assignments/index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index of My Assignments </title>
+    </head>
+    <body>
+    <table style="width:100%">
+  <tr>
+    <td>Assignments</td> 
+  </tr>
+  <tr>
+    <td><a href="hmwrk1">Homework 1</td>
+  </tr>
+</table>
+    </body>
+</html>
