ea4b7c6add2f9280e731222d3c0f393b1fd90089
Roberto Palacios <rpalaces@csumb.edu>
2017-09-14 00:05:39 +0000

    again

---
+++ labs/index.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index of My Works </title>
+    </head>
+    <body>
+    <table style="width:100%">
+  <tr>
+    <td>Labs</td>
+   
+  </tr>
+  <tr>
+    <td><a href="lab1/index.html/>Lab 1</a></td>
+    <td><a href="assignments/hmwrk1/indexassignone.html">Assignment 1</td>
+  </tr>
+</table>
+    </body>
+</html>
