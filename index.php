c79099628577c3d777448a13aad7611e889a8cba
Roberto Palacios <rpalaces@csumb.edu>
2017-09-27 23:52:22 +0000

    bleh

--- index.php
+++ index.php
@@ -1,35 +1,4 @@
-<<<<<<< HEAD
-<?php
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
-        <title>Anna Rasburn CST 336 </title>
-        <style>
-            h1{
-                font-family: 'Poiret One', cursive;
-            }
-            td{
-                font-family: 'Poiret One', cursive;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Index of Work by <em></em>Anna Rasburn</em></h1>
-        <table style="width:100%">
-          <tr>
-            <td>Labs</td>
-            <td>Homework</td> 
-          </tr>
-         <tr>
-            <td><a href="Labs/index.php">Labs</a></td>
-            <td><a href="Homework/index.php">Homework</td>
-         </tr>
-        </table>
-    </body>
-</html>
-=======
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -64,18 +33,7 @@
             getHand();
             ?>
         </div>
-        
-        
-        
-        
-       
-        
-          
-   
-                
-           
 
 
     </body>
 </html>
->>>>>>> 3c10c0829562122c1aaf5c131ab877066150fa71
